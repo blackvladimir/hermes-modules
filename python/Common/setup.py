9d8aa650be4bcf10bb8aeb5c460112b22cb4f8c4
Lukas <lukas.korous@gmail.com>
2012-02-13 22:10:47 +0100

    Work on wrapping.

--- python/Common/setup.py
+++ python/Common/setup.py
@@ -4,8 +4,11 @@ from Cython.Distutils import build_ext
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("hermes-module", [
-    "Common.pyx",
-    "Common.pxd",
-    ],language="c++",libraries=["hermes-module"])]
+    ext_modules = [Extension("hermes_module", [
+    "hermes-module.pyx",
+    "hermes-module.pxd",
+    "../translate_exceptions/translate_exception.cpp",
+    "../translate_exceptions/raise_exception.pxd",
+    "../translate_exceptions/raise_exception.pxi"
+    ],language="c++",libraries=["hermes_module"])]
 )
--- python/wrappers_generate.sh
+++ python/wrappers_generate.sh
@@ -1,4 +1,5 @@
+HERMES_DIR="/usr/local/"
 cd Common
-python setup.py build_ext --inplace --include-dirs="../C++/Common" --library-dirs="$../C++/Common"
+python setup.py build_ext --inplace --include-dirs="../C++/Common/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d" --library-dirs="$../C++/Common/:$HERMES_DIR/lib"
 cd ../01-Poisson
-python setup.py build_ext --inplace --include-dirs="$../C++/01-Poisson" --library-dirs="../C++/01-Poisson"
+python setup.py build_ext --inplace --include-dirs="$../C++/01-Poisson/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d" --library-dirs="../C++/01-Poisson/:$HERMES_DIR/lib"

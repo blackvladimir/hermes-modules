c17e6bb0498caec4294db8f1eb13760111991cd5
Lukas <lukas.korous@gmail.com>
2012-02-14 11:04:35 +0100

    Add Python install script.

---
+++ python/wrappers_install.sh
@@ -0,0 +1,7 @@
+HERMES_DIR="/usr/local"
+HERMES_WRAPPERS_DIR="/home/lukas/hpfem/hermes/l-korous/hermes-python"
+MODULES_DIR="/usr/local"
+cd Common
+python setup.py build_ext --inplace --include-dirs="$MODULES_DIR/include/hermes_module/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d/:$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common" --library-dirs="$MODULES_DIR/lib/:$HERMES_DIR/lib" --pyrex-include-dirs="$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common" install_lib
+cd ../01-Poisson
+python setup.py build_ext --inplace --include-dirs="$MODULES_DIR/include/hermes_module/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d/:$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common/:$MODULES_DIR/include/hermes_module/" --library-dirs="$MODULES_DIR/lib/:$HERMES_DIR/lib/" --pyrex-include-dirs="$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common/:../Common/" install_lib

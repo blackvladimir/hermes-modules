590c2d1c68a093a80726972403d46fc1a6594370
Lukas <lukas.korous@gmail.com>
2012-02-14 12:11:58 +0100

    Some reorganization.

--- python/wrappers_generate.sh
+++ python/wrappers_generate.sh
@@ -1,6 +1,6 @@
 HERMES_DIR="/usr/local"
 HERMES_WRAPPERS_DIR="/home/lukas/hpfem/hermes/l-korous/hermes-python"
-cd Common
-python setup.py build_ext --inplace --include-dirs="../../C++/Common/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d/:$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common" --library-dirs="../../C++/Common/:$HERMES_DIR/lib" --pyrex-include-dirs="$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common"
-cd ../01-Poisson
-python setup.py build_ext --inplace --include-dirs="../../C++/01-Poisson/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d/:$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common/:../../C++/Common/" --library-dirs="../../C++/01-Poisson/:$HERMES_DIR/lib/:../Common/" --pyrex-include-dirs="$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common/:../Common/"
+cd hermes_module
+python setup.py build_ext --inplace --include-dirs="../../C++/hermes_module/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d/:$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common" --library-dirs="../../C++/hermes_module/:$HERMES_DIR/lib" --pyrex-include-dirs="$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common"
+cd ../hermes_modulePoisson
+python setup.py build_ext --inplace --include-dirs="../../C++/hermes_modulePoisson/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d/:$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common/:../../C++/hermes_module/" --library-dirs="../../C++/hermes_modulePoisson/:$HERMES_DIR/lib/:../hermes_module/" --pyrex-include-dirs="$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common/:../hermes_module/"
--- python/wrappers_install.sh
+++ python/wrappers_install.sh
@@ -1,7 +1,7 @@
 HERMES_DIR="/usr/local"
 HERMES_WRAPPERS_DIR="/home/lukas/hpfem/hermes/l-korous/hermes-python"
 MODULES_DIR="/usr/local"
-cd Common
+cd hermes_module
 python setup.py build_ext --include-dirs="$MODULES_DIR/include/hermes_module/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d/:$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common" --library-dirs="$MODULES_DIR/lib/:$HERMES_DIR/lib" --pyrex-include-dirs="$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common" install_lib
-cd ../01-Poisson
+cd ../hermes_modulePoisson
 python setup.py build_ext --include-dirs="$MODULES_DIR/include/hermes_module/:$HERMES_DIR/include/hermes_common/:$HERMES_DIR/include/hermes2d/:$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common/:$MODULES_DIR/include/hermes_modulePoisson/" --library-dirs="$MODULES_DIR/lib/:$HERMES_DIR/lib/" --pyrex-include-dirs="$HERMES_WRAPPERS_DIR/hermes2d/:$HERMES_WRAPPERS_DIR/hermes_common/:../Common/" install_lib
